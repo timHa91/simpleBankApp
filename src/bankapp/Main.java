e13a092d0fb59526d20b4787b1a9757312b228df
Tim Hartmann <45972852+timHa91@users.noreply.github.com>
2022-01-23 16:39:57 +0100

    Update Main.java

--- src/bankapp/Main.java
+++ src/bankapp/Main.java
@@ -2,7 +2,7 @@ package bankapp;
 
 public class Main {
     public static void main(String[] args) {
-        SQLiteDB base = new SQLiteDB(args[1]);
+        SQLiteDB base = new SQLiteDB(args[0]);
         UI.printMainMenu();
         UI.mainMenuGetChoice(base);
 
